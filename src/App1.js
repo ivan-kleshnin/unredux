da7861683bff8454adb7876d7fe72ddc05a6d3f2
Ivan Kleshnin <ivan@paqmind.com>
2017-09-27 15:04:51 +0300

    An attempt to implement connection container

--- src/App1.js
+++ src/App1.js
@@ -3,7 +3,6 @@ import {Observable} from "rxjs/Observable"
 import {Subject} from "rxjs/Subject"
 
 // Import RxJS Observable functions
-import 'rxjs/add/observable/combineLatest'
 import 'rxjs/add/observable/merge'
 
 // Import RxJS Observable methods
@@ -14,8 +13,6 @@ import 'rxjs/add/operator/shareReplay'
 
 import React, { Component } from "react"
 
-import combineLatestObj from "./combineLatestObj"
-
 let intents = {
   increment: new Subject(),
   decrement: new Subject(),
@@ -24,7 +21,7 @@ let intents = {
 
 let actions = {
   increment: intents.increment.map(() => (state) => R.assoc("counter", state.counter + 1, state)),
-  decrement: intents.decrement.map(() => (state) => R.assoc("counter", state.counter - 1, state)),
+  decrement: intents.decrement.map(() => (state) => R.assoc("counter", state.counter + 1, state)),
   incrementIfOdd: intents.incrementIfOdd.map(() => (state) => {
     return R.assoc("counter", state.counter % 2 ? state.counter + 1 : state.counter, state)
   }),
@@ -34,48 +31,37 @@ let initialState = {counter: 0}
 
 let state = Observable.merge(
   actions.increment, actions.decrement, actions.incrementIfOdd,
-)
- .startWith(initialState)
- .scan((state, fn) => fn(state))
+).scan((state, fn) => fn(state), initialState)
  .distinctUntilChanged()
  .shareReplay(1)
 
-function connect(streamsToProps, Component) {
-  class Container extends Component {
-    state = {} // will be replaced with initialState on componentWillMount (before first render)
-
-    componentWillMount() {
-      let props = combineLatestObj(streamsToProps)
-      this.$ = props.subscribe((data) => {
-        this.setState(data)
-      })
-    }
-
-    componentWillUnmount() {
-      this.$.unsubscribe()
-    }
-
-    render() {
-      return React.createElement(Component, R.merge(this.props, this.state), this.props.children)
-    }
-  }
-  return Container
-}
-
 class App extends Component {
+  state = initialState
+
+  componentDidMount() {
+    this.$ = state.subscribe((state) => {
+      console.log("setting state:", state)
+      this.setState(state)
+    })
+  }
+
+  componentWillUnmount() {
+    this.$.unsubscribe()
+  }
+
   render() {
     return <div>
       <p>
-        Clicked: <span id="value">{this.props.counter}</span> times
+        Clicked: <span id="value">{this.state.counter}</span> times
         <button id="increment" onClick={() => intents.increment.next()}>+</button>
         <button id="decrement" onClick={() => intents.decrement.next()}>-</button>
         <button id="incrementIfOdd" onClick={() => intents.incrementIfOdd.next()}>Increment if odd</button>
         <button id="incrementAsync" onClick={() => {
-          setTimeout(() => intents.increment.next(), 500)
+          setTimeout(() => intents.increment.next(), 1000)
         }}>Increment async</button>
       </p>
     </div>
   }
 }
 
-export default connect({counter: state.map(s => s.counter)}, App)
+export default App
